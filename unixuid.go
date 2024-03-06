0574160b27547fb37d4bfc4f04d05c0773765676
Yinyin L <yinyinl@gmail.com>
2024-03-07 04:34:04 +0800

    migrate base type to uint32
    as UID in golang.org/x/sys/unix Ucred structure is uint32 and processing logic in os/user is operate on uint, too.

--- unixuid.go
+++ unixuid.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-type UnixUID int
+type UnixUID uint32
 
 func (uid UnixUID) MarshalText() (text []byte, err error) {
 	u, err := user.LookupId(strconv.FormatUint(uint64(uid), 10))
@@ -37,11 +37,28 @@ func MakeUIDIntMap(uidSlice []UnixUID) (uidMap map[int]struct{}) {
 	return
 }
 
+// MakeUIDIntMap returns a map of int from a slice of UnixUID.
+func MakeUIDUint32Map(uidSlice []UnixUID) (uidMap map[uint32]struct{}) {
+	uidMap = make(map[uint32]struct{})
+	for _, uid := range uidSlice {
+		uidMap[uint32(uid)] = struct{}{}
+	}
+	return
+}
+
 // CloneAsIntSlice returns a slice of int from a slice of UnixUID.
 func CloneAsIntSlice(uidSlice []UnixUID) (result []int) {
 	result = make([]int, len(uidSlice))
 	for idx, uid := range uidSlice {
 		result[idx] = int(uid)
+	}
+	return
+}
+
+func CloneAsUint32Slice(uidSlice []UnixUID) (result []uint32) {
+	result = make([]uint32, len(uidSlice))
+	for idx, uid := range uidSlice {
+		result[idx] = uint32(uid)
 	}
 	return
 }
